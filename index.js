916f82e2182c06ad3526723796fd2ede28fa940a
Lee Simon <shinoblee@gmail.com>
2018-08-22 13:23:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-   console.log('Left');
+   console.log('Right');
 }
 
 /**
