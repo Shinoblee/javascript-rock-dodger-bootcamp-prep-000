dc499344c1c0bd4e572d6cd1e822be917f6273f5
Lee Simon <shinoblee@gmail.com>
2018-08-22 13:21:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -119,7 +119,9 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-
+  document.addEventListener('keydown', function(e){
+    console.log(e.which);
+  });
    
 }
 
